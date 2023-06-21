b5a4d8e7f8b272a5ef54614810faf6c658122576
Afa Cheng <afa@afa.moe>
2023-06-21 10:41:27 +0800

    log for ac unit manager

--- BridgeService/Services/Implementations/HassMqttService.cs
+++ BridgeService/Services/Implementations/HassMqttService.cs
@@ -318,7 +318,7 @@ public partial class HassMqttService : IHostedService
             }
             else
             {
-                _logger.LogError("Received update for stale unit ({Oa}, {Ia})", oa, ia);
+                _logger.LogError("Received update for stale unit ({Oa}, {Ia}), last updated on {LastUpdated}", oa, ia, val.HasValue ? val.Value.lastUpdated.ToString("O") : "N/A");
             }
         }
         else
